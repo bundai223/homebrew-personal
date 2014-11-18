ee46b6ab5c30c9409c3d589e5da78282a3f0778e
bundai223 <bundai223@gmail.com>
2014-11-18 18:42:12 +0900

    AquaSKKのYosemite対応版

---
+++ aquaskk.rb
@@ -0,0 +1,18 @@
+class AquaSKK < Cask
+  version '4.2.2'
+  sha256 '51f8279a028b07c1dce03b34692707cbef0d438c68c02cad0aa21ad166b8c76b'
+
+  url "https://github.com/codefirst/aquaskk/releases/download/#{version}/AquaSKK-#{version}.dmg"
+  homepage 'https://github.com/codefirst/aquaskk'
+  license :unknown
+
+  pkg 'AquaSKK.pkg'
+
+  uninstall :pkgutil => 'jp.surceforge.inputmethod.aquaskk.pkg'
+#            :launchctl => 'org.macosforge.xquartz.startx',
+#  zap       :delete => [
+#                        '~/.Xauthority',
+#                       ],
+#            :rmdir => '~/.fonts'
+end
+
